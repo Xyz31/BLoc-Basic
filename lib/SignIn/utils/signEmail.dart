cd60e419c10c3117af3171da85c905a90187307d
Khan <xander2001khan@gmail.com>
2023-07-31 18:50:02 +0530

    Updated signinbloc

--- lib/SignIn/utils/signEmail.dart
+++ lib/SignIn/utils/signEmail.dart
@@ -21,120 +21,126 @@ class _SignWithEmailState extends State<SignWithEmail> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body:
-          // BlocProvider<SignInBloc>(
-          //   create: (context) => SignInBloc(),
-          //   child:
-          Container(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BlocBuilder<SignInBloc, SignInStates>(
-              bloc: SignInBloc(),
-              builder: (_, state) {
-                if (state is SignInError) {
-                  return Text(
-                    state.errorMessage,
-                    style: TextStyle(color: Colors.red),
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextField(
-              controller: _textController,
-              onChanged: (val) {
-                BlocProvider.of<SignInBloc>(context).add(SignInEventTextChanged(
-                    _textController.toString(),
-                    _passwordController.toString()));
+    return BlocProvider<SignInBloc>(
+      create: (context) => SignInBloc(),
+      child: Scaffold(
+        appBar: AppBar(),
+        body:
+            // BlocProvider<SignInBloc>(
+            //   create: (context) => SignInBloc(),
+            //   child:
+            Container(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BlocBuilder<SignInBloc, SignInStates>(
+                bloc: SignInBloc(),
+                builder: (_, state) {
+                  if (state is SignInError) {
+                    return Text(
+                      state.errorMessage,
+                      style: TextStyle(color: Colors.red),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: _textController,
+                onChanged: (val) {
+                  BlocProvider.of<SignInBloc>(context).add(
+                      SignInEventTextChanged(_textController.toString(),
+                          _passwordController.toString()));
 
-                // Use context.read to directly access the SignInEmailBloc instance
-              },
-              style: const TextStyle(
-                color: Colors.black, // Text color
-                fontSize: 14.0, // Font size
-                fontWeight: FontWeight.bold, // Font weight
-                fontStyle: FontStyle.italic, // Font style
-              ),
-              decoration: InputDecoration(
-                labelText: 'Enter your Email', // Label when the field is empty
-                // Hint text when the field is empty
-                prefixIcon: const Icon(Icons.person), // Icon before the text
-                border: OutlineInputBorder(
-                  // Rounded border
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide:
-                      const BorderSide(color: Colors.blue), // Border color
+                  // Use context.read to directly access the SignInEmailBloc instance
+                },
+                style: const TextStyle(
+                  color: Colors.black, // Text color
+                  fontSize: 14.0, // Font size
+                  fontWeight: FontWeight.bold, // Font weight
+                  fontStyle: FontStyle.italic, // Font style
                 ),
-                focusedBorder: OutlineInputBorder(
-                  // Rounded border when focused
-                  borderRadius: BorderRadius.circular(10.0),
+                decoration: InputDecoration(
+                  labelText:
+                      'Enter your Email', // Label when the field is empty
+                  // Hint text when the field is empty
+                  prefixIcon: const Icon(Icons.person), // Icon before the text
+                  border: OutlineInputBorder(
+                    // Rounded border
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.blue), // Border color
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    // Rounded border when focused
+                    borderRadius: BorderRadius.circular(10.0),
 
-                  borderSide: const BorderSide(
-                      width: 2,
-                      color: Colors.black), // Border color when focused
+                    borderSide: const BorderSide(
+                        width: 2,
+                        color: Colors.black), // Border color when focused
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextField(
-              controller: _passwordController,
-              onChanged: (val) {
-                BlocProvider.of<SignInBloc>(context).add(SignInEventTextChanged(
-                    _textController.text, _passwordController.text));
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: _passwordController,
+                onChanged: (val) {
+                  BlocProvider.of<SignInBloc>(context).add(
+                      SignInEventTextChanged(
+                          _textController.text, _passwordController.text));
 
-                // Use context.read to directly access the SignInEmailBloc instance
-              },
-              style: const TextStyle(
-                color: Colors.black, // Text color
-                fontSize: 14.0, // Font size
-                fontWeight: FontWeight.bold, // Font weight
-                fontStyle: FontStyle.italic, // Font style
-              ),
-              decoration: InputDecoration(
-                labelText:
-                    'Enter your password', // Label when the field is empty
-                // Hint text when the field is empty
-                prefixIcon: const Icon(Icons.person), // Icon before the text
-                border: OutlineInputBorder(
-                  // Rounded border
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide:
-                      const BorderSide(color: Colors.blue), // Border color
+                  // Use context.read to directly access the SignInEmailBloc instance
+                },
+                style: const TextStyle(
+                  color: Colors.black, // Text color
+                  fontSize: 14.0, // Font size
+                  fontWeight: FontWeight.bold, // Font weight
+                  fontStyle: FontStyle.italic, // Font style
                 ),
-                focusedBorder: OutlineInputBorder(
-                  // Rounded border when focused
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(
-                      width: 2,
-                      color: Colors.black), // Border color when focused
+                decoration: InputDecoration(
+                  labelText:
+                      'Enter your password', // Label when the field is empty
+                  // Hint text when the field is empty
+                  prefixIcon: const Icon(Icons.person), // Icon before the text
+                  border: OutlineInputBorder(
+                    // Rounded border
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide:
+                        const BorderSide(color: Colors.blue), // Border color
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    // Rounded border when focused
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                        width: 2,
+                        color: Colors.black), // Border color when focused
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            BlocBuilder<SignInBloc, SignInStates>(
-              builder: (context, state) {
-                return CupertinoButton(
-                    onPressed: () {},
-                    color: (state is SignInInvalid) ? Colors.blue : Colors.grey,
-                    child: const Text(
-                      'Sign In',
-                      style: TextStyle(fontSize: 20),
-                    ));
-              },
-            )
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              BlocBuilder<SignInBloc, SignInStates>(
+                builder: (context, state) {
+                  return CupertinoButton(
+                      onPressed: () {},
+                      color:
+                          (state is SignInInvalid) ? Colors.blue : Colors.grey,
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(fontSize: 20),
+                      ));
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
